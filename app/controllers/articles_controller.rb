bc8eeb3330fa7441315d2dbdb10cbaa6aa5bdaec
kimhong <hongtran131199@gmail.com>
2023-04-16 18:17:25 +0700

    Change article view and gem devise

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,5 @@
 class ArticlesController < ApplicationController
-    
-    # http_basic_authenticate_with name: "dhh", password: "secret", except: [:index, :show]
+    before_action :authenticate_user!, only: [:new, :edit, :update, :create, :destroy]
 
     def index
         @articles = Article.all
